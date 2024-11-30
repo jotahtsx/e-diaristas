7ed13c82b10aeb962b7d369a01685760b3d96083
jotahdev <joaomanoelskai@gmail.com>
2024-11-29 21:49:17 -0300

    Criando o model de repositórios pt1

---
+++ src/main/java/com/jotahdev/ediaristas/core/repositories/CleaningRepository.java
@@ -0,0 +1,9 @@
+package com.jotahdev.ediaristas.core.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jotahdev.ediaristas.core.models.CleaningService;
+
+public interface CleaningRepository extends JpaRepository<CleaningService, Long> {
+  
+}
