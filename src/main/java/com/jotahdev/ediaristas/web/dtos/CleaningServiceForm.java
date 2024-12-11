cac922199d92e9e6df9855df78cb7e9b7b546c77
jotahdev <joaomanoelskai@gmail.com>
2024-12-11 17:41:00 -0300

    Inicializando validações na view pt-2

--- src/main/java/com/jotahdev/ediaristas/web/dtos/CleaningServiceForm.java
+++ src/main/java/com/jotahdev/ediaristas/web/dtos/CleaningServiceForm.java
@@ -24,64 +24,64 @@ public class CleaningServiceForm {
   private String name;
 
   @NotNull(message = "O valor mínimo não pode ser vazio.")
-  @DecimalMin(value = "0.0", inclusive = true, message = "O valor mínimo deve ser maior ou igual a zero.")
+  @DecimalMin(value = "0.0", inclusive = true, message = "O valor mínimo deve ser maior ou igual a 0.")
   private BigDecimal minimumPrice;
 
-  @NotNull
-  @PositiveOrZero
+  @NotNull(message = "A quantidade de horas não pode ser vazia.")
+  @PositiveOrZero(message = "A quantidade de horas deve ser maior ou igual a 0.")
   private Integer quantityOfHours;
 
-  @NotNull
-  @PositiveOrZero
-  @Max(100)
+  @NotNull(message = "A porcentagem da comissão não pode ser vazia.")
+  @PositiveOrZero(message = "A porcentagem da comissão deve ser maior ou igual a 0.")
+  @Max(value = 100, message = "A porcentagem da comissão não pode ser maior que 100%.")
   private BigDecimal commissionPercentage;
 
-  @NotNull
-  @PositiveOrZero
+  @NotNull(message = "É preciso setar um valor de horas por quarto.")
+  @PositiveOrZero(message = "A quantidade de horas por quarto deve ser maior ou igual a 0.")
   private BigDecimal roomHours;
 
-  @NotNull
-  @PositiveOrZero
+  @NotNull(message = "Informe o valor do quarto.")
+  @PositiveOrZero(message = "O valor deve ser 0 ou mais.")
   private BigDecimal roomPrice;
 
-  @NotNull
-  @PositiveOrZero
+  @NotNull(message = "É preciso setar um valor de horas por sala.")
+  @PositiveOrZero(message = "A quantidade de horas por sala deve ser maior ou igual a 0.")
   private BigDecimal livingAreaHours;
 
-  @NotNull
-  @PositiveOrZero
+  @NotNull(message = "Informe o valor da sala.")
+  @PositiveOrZero(message = "O valor deve ser 0 ou mais.")
   private BigDecimal livingAreaPrice;
 
-  @NotNull
-  @PositiveOrZero
+  @NotNull(message = "É preciso setar um valor de horas por banheiro.")
+  @PositiveOrZero(message = "A quantidade de horas por sala deve ser maior ou igual a 0.")
   private BigDecimal bathroomHours;
 
-  @NotNull
-  @PositiveOrZero
+  @NotNull(message = "Informe o valor do banheiro.")
+  @PositiveOrZero(message = "O valor deve ser 0 ou mais.")
   private BigDecimal bathroomPrice;
 
-  @NotNull
-  @PositiveOrZero
+  @NotNull(message = "É preciso setar um valor de horas por cozinha.")
+  @PositiveOrZero(message = "A quantidade de horas por cozinha deve ser maior ou igual a 0.")
   private BigDecimal kitchenHours;
 
-  @NotNull
-  @PositiveOrZero
+  @NotNull(message = "Informe o valor da cozinha.")
+  @PositiveOrZero(message = "O valor deve ser 0 ou mais.")
   private BigDecimal kitchenPrice;
 
-  @NotNull
-  @PositiveOrZero
+  @NotNull(message = "É preciso setar um valor de horas por quintal.")
+  @PositiveOrZero(message = "A quantidade de horas por quintal deve ser maior ou igual a 0.")
   private BigDecimal yardHours;
 
-  @NotNull
-  @PositiveOrZero
+  @NotNull(message = "Informe o valor do quintal.")
+  @PositiveOrZero(message = "O valor deve ser 0 ou mais.")
   private BigDecimal yardPrice;
 
-  @NotNull
-  @PositiveOrZero
+  @NotNull(message = "Insira o número de horas para serviços adicionais.")
+  @PositiveOrZero(message = "As horas devem ser 0 ou mais.")
   private BigDecimal othersHours;
 
-  @NotNull
-  @PositiveOrZero
+  @NotNull(message = "Defina o valor dos serviços adicionais.")
+  @PositiveOrZero(message = "O valor deve ser 0 ou mais.")
   private BigDecimal othersPrice;
 
   @NotNull
