6398d169293f082a9ccd0ba0ce51594ab424a8d1
jotahdev <joaomanoelskai@gmail.com>
2024-12-13 11:31:19 -0300

    Inicializando o modelo de usuários

---
+++ src/main/java/com/jotahdev/ediaristas/core/enums/TypeOfUser.java
@@ -0,0 +1,7 @@
+package com.jotahdev.ediaristas.core.enums;
+
+public enum TypeOfUser {
+  ADMIN,
+  CUSTOMER,
+  CLEANER
+}
---
+++ src/main/java/com/jotahdev/ediaristas/core/models/User.java
@@ -0,0 +1,43 @@
+package com.jotahdev.ediaristas.core.models;
+
+import com.jotahdev.ediaristas.core.enums.TypeOfUser;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
+public class User {
+  
+  @EqualsAndHashCode.Include
+  @ToString.Include
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "full_name", nullable = false)
+  private String fullName;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(name = "type_of_user", length = 8, nullable = false)
+  @Enumerated(EnumType.STRING)
+  private TypeOfUser typeOfUser;
+
+}
